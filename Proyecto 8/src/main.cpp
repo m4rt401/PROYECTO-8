1a72eb9af3d1021e5663c615ac4b996e7f9f8a2d
m4rt401 <79660030+m4rt401@users.noreply.github.com>
2021-05-30 21:43:48 +0200

    Add files via upload

---
+++ Proyecto 8/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(115200);
+  Serial2.begin(115200);
+}
+
+void loop() {
+  if(Serial.available()){
+    Serial2.write(Serial.read());
+    delay(2);
+    if(Serial2.available()){
+      Serial.write(Serial2.read());
+    } 
+  }
+}
